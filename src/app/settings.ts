3141f274095b04250d14d944d91be8d854a9122d
Florian Aucomte <f.aucomte@hotmail.co.uk>
2025-05-30 11:57:39 +0100

    Upgrade to Node 22 (#3)

--- src/app/settings.ts
+++ src/app/settings.ts
@@ -2,5 +2,7 @@ import env from "codeforlife/settings"
 
 export * from "codeforlife/settings"
 
-// Example of how to get an environment variable.
-export const EXAMPLE = env.VITE_EXAMPLE ?? "DEFAULT_VALUE"
+export const {
+  // Example of how to get an environment variable.
+  EXAMPLE = "DEFAULT_VALUE",
+} = env.vite
