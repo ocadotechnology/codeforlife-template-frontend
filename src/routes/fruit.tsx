b5cb85e5a1dd397c69301f9d86a94e73baaa76d3
SKairinos <skairinos@gmail.com>
2025-09-11 12:49:57 +0000

    new cfl package

--- src/routes/fruit.tsx
+++ src/routes/fruit.tsx
@@ -1,4 +1,4 @@
-import { Route } from "react-router-dom"
+import { Route } from "react-router"
 
 import FruitDetail from "../pages/fruitDetail/FruitDetail"
 import FruitList from "../pages/fruitList/FruitList"
--- src/routes/general.tsx
+++ src/routes/general.tsx
@@ -1,4 +1,4 @@
-import { Route } from "react-router-dom"
+import { Route } from "react-router"
 
 import Home from "../pages/home/Home"
 import paths from "./paths"
